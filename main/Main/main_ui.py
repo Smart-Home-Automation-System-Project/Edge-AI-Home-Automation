4113fbdefb55af6ff9fa87a186dc720a704983ec
Malaka Gunawardana <malakagunawardana2002@gmail.com>
2025-04-21 20:26:43 +0530

    System Structure Updated
    Added web interface
    Updated modules, database, file structure

--- main/Main/main_ui.py
+++
@@ -1,9 +0,0 @@
-from web.server import init_webui
-
-if __name__ == "__main__":
-    init_webui()
-    try:
-        while True:
-            pass  # Keep the main thread alive
-    except KeyboardInterrupt:
-        print("Stopped.")
--- root/src/utils/mqtt.py
+++ root/src/utils/mqtt.py
@@ -3,7 +3,7 @@ from utils.utils import *
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(dotenv_path='config/.env')
 MQTT_USERNAME = os.getenv("MQTT_USERNAME")
 MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
 MQTT_PORT = os.getenv("MQTT_PORT")
@@ -12,9 +12,9 @@ class MQTTConnection:
     _client = None
 
     @staticmethod
-    def get_client():
+    def get_client(client_id="central_main"):
         if MQTTConnection._client is None:
-            MQTTConnection._client = mqtt.Client(client_id="central_main")  # Set your own ID
+            MQTTConnection._client = mqtt.Client(client_id=client_id)  # Set your own ID
             MQTTConnection._client.username_pw_set(str(MQTT_USERNAME), str(MQTT_PASSWORD))
             MQTTConnection._client.connect(get_local_ip(), int(MQTT_PORT))
             MQTTConnection._client.loop_start()  # Run network loop in background
