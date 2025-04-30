a6501c08f8a586499fd693def9c63b5946a54b3d
Malaka Gunawardana <malakagunawardana2002@gmail.com>
2025-04-30 23:01:38 +0530

    Add virtual module for demo
    Updated sensor control system
    Virtual module for demo
    Updated UI

--- root/src/run_mqtt.bat
+++ root/src/run_mqtt.bat
@@ -1,3 +1,13 @@
+@echo off
+
+color A
+
+echo ===================================================
+echo AI Home Automation System - MQTT BROKER
+echo Version: 1.0.0 (Stable)
+echo Status: Running...
+echo ===================================================
+
 mosquitto -c ".\config\mosquitto.conf" -v
 
 pause
